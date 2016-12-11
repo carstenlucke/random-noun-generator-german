bd9b1977b10d55370cd1e053436ce5b52a7a3b1e
Carsten Lucke <clucke@thmmbp.fritz.box>
2016-12-11 12:46:56 +0100

    just cleanup and some tagging

--- runtime.js
+++ runtime.js
@@ -1,4 +1,4 @@
-var now = require("performance-now")
+var now = require('performance-now');
 
 // the language file contains 102.493 nouns which are loaded 
 // into an array (thus into memory) when require('random-noun-generator-german')
@@ -6,21 +6,25 @@ var now = require("performance-now")
 
 // let's see how long this takes ...
 
-var t0 = now();
-var rndWord = require('./src/generator');
-var t1 = now();
-var word = rndWord();
-var t2 = now();
+var t0;
+var t1;
+var t2;
 
-console.log("Call to require('random-noun-generator-german') took " + (t1 - t0).toFixed(3) + " milliseconds.")
-console.log("Call to generate a random noun (" + word + ") took " + (t2 - t1).toFixed(3) + " milliseconds.")
-console.log("... thus, loading \"random-noun-generator-german\" and generating one random noun took " + (t2 - t0).toFixed(3) + " milliseconds.\n")
+t0 = now();
+var rndWord = require('./src/generator');
+t1 = now();
+var word = rndWord();
+t2 = now();
+
+console.log("Call to require('random-noun-generator-german') took " + (t1 - t0).toFixed(3) + " milliseconds.");
+console.log("Call to generate a random noun (" + word + ") took " + (t2 - t1).toFixed(3) + " milliseconds.");
+console.log("... thus, loading \"random-noun-generator-german\" and generating one random noun took " + (t2 - t0).toFixed(3) + " milliseconds.\n");
 
 console.log("Still proceeding ... generating ten random nouns: ");
 for (var i = 0; i < 10; i++) {
-    var t0 = now();
-    var word = rndWord();
-    var t1 = now();
+    t0 = now();
+    word = rndWord();
+    t1 = now();
 
-    console.log(i+1 + ": <" + word + "> " + (t1 - t0).toFixed(3) + " milliseconds.")
+    console.log(i+1 + ": <" + word + "> " + (t1 - t0).toFixed(3) + " milliseconds.");
 }
